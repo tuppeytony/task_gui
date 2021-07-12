695e3b50c1380f1022a0abee13781751d216b3b7
tuppeytony <miro_19@mail.ru>
2021-07-12 21:02:23 +0300

    start my first gui app

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5 import QtWidgets
+import design
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_mainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
