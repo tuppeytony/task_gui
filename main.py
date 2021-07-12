61055290506a4a336abc82166d5b3a751dd3dedd
tuppeytony <miro_19@mail.ru>
2021-07-12 21:46:49 +0300

    add new requarements, start to develop app

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_mainWindow):
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton_2.clicked.connect(self.add_task)
+
+    def save(self):
+        pass
+
+    def add_task(self):
+        # получение значения поля
+        all_inputs = self.lineEdit.text() and self.lineEdit_2.text()
+        if all_inputs:
+            print('Valid')
+        else:
+            print('Not valid!')
 
 
 def main():
